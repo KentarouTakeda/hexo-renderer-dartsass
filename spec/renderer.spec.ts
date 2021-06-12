78ac654d327d69d64939ff672da854b714b6b2f6
武田 憲太郎 <takeda@youmind.jp>
2021-06-12 16:21:51 +0900

    config変更のテスト

--- spec/renderer.spec.ts
+++ spec/renderer.spec.ts
@@ -2,7 +2,7 @@ import Hexo = require('hexo');
 import { make } from '../src/renderer';
 
 describe('Function `make`', ()=>{
-    it('compile scss', async () => {
+    it('compile scss with default settings', async () => {
         const hexo = new Hexo();
         const data: Hexo.extend.RendererData = {
             path: __dirname + '/files/default.scss',
@@ -10,5 +10,28 @@ describe('Function `make`', ()=>{
         };
         const result = await make.call(hexo, data, {});
         expect(result).toBe('html body {\n  width: 100%;\n}')
+    });
+
+    it('compile scss with custom config', async () => {
+        const hexo = new Hexo();
+        hexo.config.sass = { indentWidth: 4 };
+        const data: Hexo.extend.RendererData = {
+            path: __dirname + '/files/default.scss',
+            text: ''
+        };
+        const result = await make.call(hexo, data, {});
+        expect(result).toBe('html body {\n    width: 100%;\n}')
+    });
+
+    it('compile scss with custom theme config', async () => {
+        const hexo = new Hexo();
+        hexo.config.sass = { indentWidth: 1 };
+        hexo.theme.config.sass = { indentWidth: 4 };
+        const data: Hexo.extend.RendererData = {
+            path: __dirname + '/files/default.scss',
+            text: ''
+        };
+        const result = await make.call(hexo, data, {});
+        expect(result).toBe('html body {\n width: 100%;\n}')
     });
 });
