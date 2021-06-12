49bbfbdf2dcd6e078eb66978825b557cb7c41b51
武田 憲太郎 <takeda@youmind.jp>
2021-06-12 11:48:55 +0900

    `_config.yml` の設定をsassのコンパイルオプションにそのまま渡すよう修正

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,9 +1,16 @@
 import Hexo = require('hexo');
 import sass = require('sass');
 
-export const make = function(data: Hexo.extend.RendererData, options: Object){
+export const make = function(this: Hexo, data: Hexo.extend.RendererData, options: Object){
+
+    const config = Object.assign(
+        this.theme.config.sass || {},
+        this.config.sass || {},
+        {file: data.path}
+    );
+
     return new Promise<string>((resolve, reject)=>{
-        sass.render({file: data.path}, (err, result) => {
+        sass.render(config, (err, result) => {
             if(err) {
                 reject(err);
             }
