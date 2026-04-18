8e6953dbdd7d2e78afbbc3a5f4770e1c27414650
武田 憲太郎 <takeda@youmind.jp>
2026-04-18 23:44:18 +0900

    refactor: update source and tests for hexo 7 and TypeScript 6
    - Use StoreFunctionData instead of Hexo.extend.RendererData
    - Extract getProperty to module scope
    - Fix hasOwnProperty to use Object.prototype.hasOwnProperty.call
    - Align import style with hexo 7 exports

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,42 +1,38 @@
-import Hexo from 'hexo';
+import Hexo = require('hexo');
 import sass = require('sass');
+import type { StoreFunctionData } from 'hexo/dist/extend/renderer';
 
-export const make = async function (this: Hexo, data: Hexo.extend.RendererData, options: {
-    [key: string]: any
-}) {
+function getProperty(obj: any, name: string) {
+    name = name.replace(/\[(\w+)]/g, '.$1').replace(/^\./, '');
+
+    const split = name.split('.');
+    let key = split.shift();
+
+    if (!key || !Object.prototype.hasOwnProperty.call(obj, key)) return '';
+    let result = obj[key];
+    const len = split.length;
+
+    if (!len) return result || '';
+    if (typeof result !== 'object') return '';
+
+    for (let i = 0; i < len; i++) {
+        key = split[i];
+        if (!Object.prototype.hasOwnProperty.call(result, key)) return '';
+
+        result = result[split[i]];
+        if (typeof result !== 'object') return result;
+    }
+
+    return result;
+}
+
+export const make = async function (this: Hexo, data: StoreFunctionData, options: object) {
     if(data.path == null) {
         return '';
     }
 
     const self = this;
     const themeCfg = self.theme.config || {};
-
-    function getProperty(obj: any, name: string) {
-        name = name.replace(/\[(\w+)]/g, '.$1').replace(/^\./, '');
-
-        const split = name.split('.');
-        let key = split.shift();
-
-        if (!obj.hasOwnProperty(key)) return '';
-        if (!key) {
-            return '';
-        }
-        let result = obj[key];
-        const len = split.length;
-
-        if (!len) return result || '';
-        if (typeof result !== 'object') return '';
-
-        for (let i = 0; i < len; i++) {
-            key = split[i];
-            if (!result.hasOwnProperty(key)) return '';
-
-            result = result[split[i]];
-            if (typeof result !== 'object') return result;
-        }
-
-        return result;
-    }
 
     const config: sass.Options<'async'> = Object.assign(
         this.theme.config.sass || {},
